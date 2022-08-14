7e5e88598837a11fb7fb94d53fed9fad7045361f
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-14 17:14:14 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -9,10 +9,13 @@ from collections import Counter, OrderedDict
 import copy as cp
 from sklearn.decomposition import LatentDirichletAllocation as LDiA
 import gensim
+from PIL import Image
 import streamlit as st  # For the web app
 #%%
+img1=Image.open('testimg.png')
 with st.sidebar:
     file = st.file_uploader ("Click “Browse files” to upload files", type=["csv","xlsx", "xls"])
     st.write("You uploaded:", file)
     st.write('Please upload a file no larger than 200MB')
     st.write('The file must be a .csv,.xls or .xlsx file')
+    st.image(img1, width=200)
