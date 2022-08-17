c2a0f7e01f3ac2f7383577b0df32c6d35596461f
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-17 14:49:01 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -42,6 +42,7 @@ with st.sidebar:
     st.download_button('download sample data in English', data=english_example, file_name='sample data in English.csv',
                        mime='csv')
     st.download_button('下载中文示例数据', data=chinese_example, file_name='中文示例数据.csv', mime='csv')
+    st.write('You can understand the workflow of this program by uploading sample data.')
     st.write('Note: When the program is running, there will be a little man doing sports in the upper right corner of the web page,don\`t refresh this page or do anything else until he stops.')
 # %%
 #测试区
