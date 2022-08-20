72f8a3ff1c437e618356e78f7ca5e2fb864e9ad0
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-20 08:36:51 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -345,7 +345,7 @@ if file != None:
 import time
 with tab6:
     with st.empty():
-        for seconds in range(60):
+        for seconds in range(10):
             st.write(f"⏳ {seconds} seconds have passed")
             time.sleep(1)
     st.write("✔️ 1 minute over!")
