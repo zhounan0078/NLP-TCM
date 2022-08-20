26d9c86e7c77fd4e0daf7b7e76da2b0f6ff2225d
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-20 08:37:57 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -344,6 +344,22 @@ if file != None:
 
 import time
 with tab6:
+    placeholder = st.empty()
+
+    # Replace the placeholder with some text:
+    placeholder.text("Hello")
+
+    # Replace the text with a chart:
+    placeholder.line_chart({"data": [1, 5, 2, 6]})
+
+    # Replace the chart with several elements:
+    with placeholder.container():
+        st.write("This is one element")
+        st.write("This is another")
+
+    # Clear all those elements:
+    placeholder.empty()
+    
     with st.empty():
         for seconds in range(10):
             st.write(f"⏳ {seconds} seconds have passed")
