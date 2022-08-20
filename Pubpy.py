3b2675cc9742258b7cfb44f1aab0967977307764
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-20 10:50:11 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -39,13 +39,13 @@ english_example = convert_df(out1)
 chinese_example = convert_df(out2)
 #定义文件读取函数
 def txt_read(file):
-    try:
+    if file != None:
         txt = pd.read_csv(file)
         txt = pd.DataFrame(txt)
         col = txt.columns
         txt = txt.set_index(col[0])
         return txt
-    except:
+    else:
         st.write("Please upload a file")
         txt = pd.DataFrame(out1)
         col = txt.columns
