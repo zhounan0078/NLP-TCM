08f906e26461a2a45172348ffb8035a8103f1606
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-21 20:47:40 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -298,7 +298,9 @@ with tab2:
         ax2.set_title('Dot product')
         st.pyplot(fig2)
         fig3, ax3 = plt.subplots()
-        sns.heatmap(cos_dot_df, annot=True,fmt=".2g", linewidths=.5, cmap='YlGnBu')
+        sns.heatmap(cos_dot_df, annot=True,fmt=".2g", linewidths=.5, cmap='YlGnBu',fontproperties=font)
+        plt.ylabel('herbs', fontproperties=font)
+        plt.yticks(x, fontproperties=font)
         ax3.set_title('Cosine similarity')
         st.pyplot(fig3)
 
