e08db24515d67c58311537fdba0991c27ad417e8
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-21 21:04:45 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -300,7 +300,7 @@ with tab2:
         fig3, ax3 = plt.subplots()
         sns.heatmap(cos_dot_df, annot=True,fmt=".2g", linewidths=.5, cmap='YlGnBu')
         plt.ylabel('herbs', fontproperties=font)
-        plt.yticks(select_result, fontproperties=font)
+        
         ax3.set_title('Cosine similarity')
         st.pyplot(fig3)
 
