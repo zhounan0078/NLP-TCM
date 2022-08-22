2c09ebe8e686dc416025805c5fa54d7ffaafb44a
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-22 09:47:01 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -300,7 +300,7 @@ with tab2:
         sns.heatmap(dense_dot_df, annot=True,fmt=".2g", linewidths=.5, cmap='YlOrRd')
         ax2.set_title('Dot product')
         plt.yticks(list(dense_dot_df.index), fontproperties=font)
-        st.pyplot(sns)
+        st.pyplot(fig2)
 
         fig3, ax3 = plt.subplots()
         sns.heatmap(cos_dot_df, annot=True,fmt=".2g", linewidths=.5, cmap='YlGnBu')
