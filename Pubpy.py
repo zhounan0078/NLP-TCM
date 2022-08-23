bc7553133ca5a85952a3d65e0615d5c2b10610a5
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-23 15:31:52 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -399,6 +399,7 @@ with tab5:
         plt.axvline(x=x[y.index(min(y))], c='r', ls='--', lw=2)
         #plt.show()
         st.pyplot(fig4)
+        st.write('hhahahahah')
         with st.expander("See explanation"):
             st.write('Perplexity is an important reference indicator for determining the number of topics in the LDiA model. When the downward trend of perplexity begins to flatten, it is the number of topics we need to keep')
         #else:
