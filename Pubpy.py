19beae2df5ef070658f295ee53257ba2fa6dc09f
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-23 16:22:52 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -37,11 +37,11 @@ def convert_df(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, index=False, sheet_name='Sheet1')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
+    #workbook = writer.book
+    #worksheet = writer.sheets['Sheet1']
     #format1 = workbook.add_format({'num_format': '0.00'})
     #worksheet.set_column('A:A', None, format1)
-    #writer.save()
+    writer.save()
     processed_data = output.getvalue()
     return processed_data
 
