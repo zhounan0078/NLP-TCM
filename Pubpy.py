f1d496ba5861450065dcc2c901df34e618736102
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-23 16:21:26 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from io import BytesIO
+from XlsxWriter import Workbook
 from pyxlsb import open_workbook as open_xlsb
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -38,9 +39,9 @@ def convert_df(df):
     df.to_excel(writer, index=False, sheet_name='Sheet1')
     workbook = writer.book
     worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'})
-    worksheet.set_column('A:A', None, format1)
-    writer.save()
+    #format1 = workbook.add_format({'num_format': '0.00'})
+    #worksheet.set_column('A:A', None, format1)
+    #writer.save()
     processed_data = output.getvalue()
     return processed_data
 
