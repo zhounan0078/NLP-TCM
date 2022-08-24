80d0707cb43dd2b1379dc287a4101e0e5b743adb
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-24 15:00:48 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -339,10 +339,10 @@ with tab3:
     tf_idf_sort = tf_idf_sort.set_index("herb")
     tab3_col1,tab3_col2 = st.columns(2)
     with tab3_col1:
-        st.write('Top 10 most important herbs')
-        st.table(tf_idf_sort.head(10))
-        plt.bar(tf_idf_sort.head(10))
-        st.pyplot(plt)
+        #st.write('Top 10 most important herbs')
+        #st.table(tf_idf_sort.head(10))
+        #plt.bar(tf_idf_sort.head(10))
+        #st.pyplot(plt)
     with tab3_col2:
         st.write('Bottom 10 most important herbs')
         st.table(tf_idf_sort.tail(10))
