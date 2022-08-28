7789258be46b691bcb2340f86de414beeb89862a
zhounan0078 <111061293+zhounan0078@users.noreply.github.com>
2022-08-28 09:27:50 +0800

    Update Pubpy.py

--- Pubpy.py
+++ Pubpy.py
@@ -136,10 +136,11 @@ with tab1:
             st.write('The most common herb is: ')
             st.table(most_common_herb1)
         with tab1_col2:
+            st.write('  ')
         # 作图
             if most_common_herb1.empty == False:
                 most_common_herb1 = most_common_herb1.set_index('herb')
-                st.bar_chart(most_common_herb1,use_container_width=True,height=600)
+                st.bar_chart(most_common_herb1,use_container_width=True,height=600,)
                 fig1, ax1 = plt.subplots()
                 x = most_common_herb1['herb']
                 y = most_common_herb1['count']
